0aa2457071a95d969b901ac5b14b24818cde46f3
Hongmin010 <80451489+Hongmin010@users.noreply.github.com>
2025-07-26 16:26:40 +0900

    [Gold IV] Title: 호텔, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1106. 호텔/호텔.cc
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int N,c,minn=10000000,minC;
+pair<int,int> hotel[21];
+int money[1100];
+int main(void){
+    cin>>c>>N;
+    for(int i=0;i<N;i++){
+        cin>>hotel[i].second>>hotel[i].first;
+    }
+    for(int i=0;i<1100;i++) money[1100]=11111111;
+    sort(hotel,hotel+N);
+    for(int i=1;i<c+100;i++){
+        minC=10000000;
+        for(int j=0;j<N;j++){
+            if(i-hotel[j].first<0) break;
+            if(minC>money[i-hotel[j].first]+hotel[j].second) minC=money[i-hotel[j].first]+hotel[j].second;
+        }
+        money[i]=minC;
+    }
+    for(int i=c;i<c+100;i++){
+        if(minn>money[i]) minn=money[i];
+    }
+    cout<<minn;
+    return 0;
+}
+
+
+
+
+
+
